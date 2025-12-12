2950f895d11b38b26dcf77c05cc14a5cae98e096
BregNights <daniel_baumann567@hotmail.com>
2025-12-12 15:17:43 -0300

    fix

--- src/application/repositories/short-url-repository.ts
+++
@@ -1,6 +0,0 @@
-import { ShortUrl } from "@/enterprise/entities/short-url"
-
-export interface ShortUrlRepository {
-  create(shortUrl: ShortUrl): Promise<ShortUrl>
-  updateShortCode(id: string, code: string): Promise<ShortUrl>
-}
