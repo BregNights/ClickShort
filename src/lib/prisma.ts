28fa3aefa3de1018dad189287a703d99a593acd3
BregNights <daniel_baumann567@hotmail.com>
2025-12-15 19:03:02 -0300

    update prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,3 @@
-import { env } from "@/env"
 import { PrismaClient } from "generated/prisma"
 
-export const prisma = new PrismaClient({
-  log: env.NODE_ENV === "dev" ? ["query"] : [],
-})
+export const prisma = new PrismaClient()
