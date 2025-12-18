0f6a74ffd512070d0033ec9edaa5dbbbe753e9b0
BregNights <daniel_baumann567@hotmail.com>
2025-12-18 08:16:35 -0300

    added error handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import fastify from "fastify"
+import { ZodError } from "zod"
 import { clickShort } from "./infra/http/routes/click-short.routes"
 import { registerDeleteExpiredShortCodesJob } from "./infra/jobs/delete-expired-short-codes.job"
 
@@ -7,3 +8,13 @@ export const app = fastify()
 registerDeleteExpiredShortCodesJob()
 
 app.register(clickShort)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation error.", issues: error.format() })
+  }
+
+  return reply.status(500).send({ message: "internal server error." })
+})
